c513319a59fe34b136dcf12f37c2848a0244724f
SvInTyS <an.al.zaitsev@yandex.ru>
2025-12-01 03:48:48 +0300

    init: Spring Boot 4.0.0 project setup

---
+++ src/main/java/com/webstore/webstore/SystemsEngineeringSpringApplication.java
@@ -0,0 +1,13 @@
+package com.webstore.webstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemsEngineeringSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SystemsEngineeringSpringApplication.class, args);
+    }
+
+}
